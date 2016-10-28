96bd631c935f3ba597f745188c8a1662c7575997
ArnaudM <arnaud.mathias@signall.com>
2016-10-28 18:24:23 +0200

    added some examples

--- visionect-examples.js
+++ visionect-examples.js
@@ -1,16 +1,55 @@
-var api = require('./visionect-api.js');
-
-var config = {
-    apiKey: '',
-    apiSecret: '',
-    host: '',
-    port: ''
-}
+var api = require('./visionect-api');
+var config = require('./config');
 
 api.setConfig(config);
 
-// get server config
+var uuid = '46004900-1351-3432-3434-373300000000';
 
-api.getVisionectConfig().then(function (response) {
+/**
+ var uuid = 'your_uuid';
+
+ // add a user
+
+ sjcl = require('sjcl'),
+ user = "Abitbol",
+ password = "faitrêverlesménagères",
+ salt = "saufquemoi";
+ // or, less funny salt = sjcl.codec.hex.fromBits(sjcl.random.randomWords(2));
+ var enc_pass = salt + ':' + sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(salt + ':' + user + ':' + password))
+
+ var newUser = {
+    "Username": user,
+    "Password": enc_pass,
+    "IsActive": true,
+    "IsAPI": false
+}
+
+ api.createUser(newUser).then(function (response) {
     console.log(response);
 })
+
+ // remove a user
+
+ api.removeUser('Abitbol').then(function (response) {
+    console.log(response);
+});
+
+ // update a device
+
+ api.getDevice(uuid).then(function (device) {
+    device = JSON.parse(device);
+    device["Options"]["Name"] = "newDeviceName";
+    api.updateDevice(uuid, device).then(function (response) {
+        console.log(reponse["Options"]["Name"]);
+    });
+})
+
+ // get device status
+
+ var dateFrom = Math.floor(Date.now() / 1000) - 240,
+ dateTo = Math.floor(Date.now() / 1000);
+
+ api.getDeviceStatus(uuid, dateFrom, dateTo, true).then(function (response) {
+    console.log(response);
+})
+ **/
--- visionect-methods.js
+++ visionect-methods.js
@@ -1,9 +1,9 @@
 var visionectMethods = {
-  visionectGetMethod : visionectGetMethod,
-  visionectPutMethod : visionectPutMethod,
-  visionectPostMethod : visionectPostMethod,
-  visionectDeleteMethod : visionectDeleteMethod,
-  visionectSendImage : visionectSendImage
+    visionectGetMethod: visionectGetMethod,
+    visionectPutMethod: visionectPutMethod,
+    visionectPostMethod: visionectPostMethod,
+    visionectDeleteMethod: visionectDeleteMethod,
+    visionectSendImage: visionectSendImage
 }
 
 module.exports = visionectMethods;
@@ -14,126 +14,126 @@ var helper = require('./visionect-helper'),
     FormData = require('form-data'),
     fs = require('fs');
 
-function visionectRequest(path,method,contentType,body,expectedHttpCode){
-  var method = method ? method : 'GET',
-      contentType = contentType ? contentType : 'application/json',
-      expectedHttpCode = expectedHttpCode ? expectedHttpCode : 200,
-      date = Date(),
-      // strip get arguments from path
-      cleanPath = (path.indexOf('?') > -1 ) ? path.substr(0, path.indexOf('?')) : path;
+function visionectRequest(path, method, contentType, body, expectedHttpCode) {
+    var method = method ? method : 'GET',
+        contentType = contentType ? contentType : 'application/json',
+        expectedHttpCode = expectedHttpCode ? expectedHttpCode : 200,
+        date = Date(),
+        // strip get arguments from path
+        cleanPath = (path.indexOf('?') > -1 ) ? path.substr(0, path.indexOf('?')) : path;
 
-      var authorization = helper.getAuthorization(cleanPath,method,contentType,date);
+    var authorization = helper.getAuthorization(cleanPath, method, contentType, date);
 
-      var headers = {
-        'content-type' : contentType,
-        'Date' : date,
-        'Authorization' : authorization
-      };
+    var headers = {
+        'content-type': contentType,
+        'Date': date,
+        'Authorization': authorization
+    };
 
-      body = body ? JSON.stringify(body) : null;
+    body = body ? JSON.stringify(body) : null;
 
-      var request = http.request({
-         method: method,
-         host: helper.getHost(),
-         port: helper.getPort(),
-         path: path,
-         headers: headers
-      });
-      request.end(body);
+    var request = http.request({
+        method: method,
+        host: helper.getHost(),
+        port: helper.getPort(),
+        path: path,
+        headers: headers
+    });
+    request.end(body);
 
-console.log(method,helper.getHost(),helper.getPort(),path,body,authorization,date);
+    console.log(method, helper.getHost(), helper.getPort(), path, body, authorization, date);
 
     var promise = new Promise(
-      function(resolve, reject) {
+        function (resolve, reject) {
 
-        request.on('response', function (response) {
-           var responseBody = '';
-           if (response.statusCode  != expectedHttpCode) {
-              resolve(('Error: ' + response.statusCode));
-           }
-          response.on('data', function (chunk) {
-            responseBody += chunk;
-          });
-          response.on('end', function () {
-              resolve(responseBody);
-          });
-        });
-      }
+            request.on('response', function (response) {
+                var responseBody = '';
+                if (response.statusCode != expectedHttpCode) {
+                    resolve(('Error: ' + response.statusCode));
+                }
+                response.on('data', function (chunk) {
+                    responseBody += chunk;
+                });
+                response.on('end', function () {
+                    resolve(responseBody);
+                });
+            });
+        }
     );
     return promise;
 
 }
 
-function visionectSendImage(uuid,imageFile){
-  var imageStream = fs.createReadStream(imageFile);
+function visionectSendImage(uuid, imageFile) {
+    var imageStream = fs.createReadStream(imageFile);
 
-  var form = new FormData();
-  form.append('image', imageStream);
-  var headers = form.getHeaders();
-  date = Date(),
-  path = util.format('/backend/%s', uuid),
-  method = 'PUT',
-  contentType = 'image/png';
-  headers.Date = date;
-  headers.Authorization =  helper.getAuthorization(path,method,contentType,date);
-
-
-  var request = http.request({
-     method: method,
-     host: helper.getHost(),
-     port: helper.getPort(),
-     path: path,
-     headers: headers
-  });
-
-  // console.log(util.inspect(form, {showHidden: false, depth: null}));
-
-  console.log('uuid : ',uuid);
-  console.log('host : ',helper.getHost());
-  console.log('content-type : ',headers['content-type']);
-  console.log('apiKey : ',helper.getApiKey());
-  console.log('apiSecret : ',helper.getApiSecret());
-  console.log('requete url : ',helper.getHost()+':'+'8081'+path);
-  console.log('headers : ',util.inspect(headers, {showHidden: false, depth: null}));
+    var form = new FormData();
+    form.append('image', imageStream);
+    var headers = form.getHeaders();
+    date = Date(),
+        path = util.format('/backend/%s', uuid),
+        method = 'PUT',
+        contentType = 'image/png';
+    headers.Date = date;
+    headers.Authorization = helper.getAuthorization(path, method, contentType, date);
 
 
-  var promise = new Promise(
-    function(resolve, reject) {
+    var request = http.request({
+        method: method,
+        host: helper.getHost(),
+        port: helper.getPort(),
+        path: path,
+        headers: headers
+    });
 
-      form.pipe(request);
+    // console.log(util.inspect(form, {showHidden: false, depth: null}));
 
-      request.on('response', function (res) {
-         var body = '';
-         if (res.statusCode  != 200) {
-            reject('Error: ' + res.statusCode)
-         }
-        imageStream.on('error', function(err) {
-          res.end('error imageStream : ',err);
-        });
-        res.on('data', function (chunk) {
-          body += chunk;
-        });
-        res.on('end', function () {
-          resolve(body);
-        });
-      });
-    }
-  );
-  return promise;
+    console.log('uuid : ', uuid);
+    console.log('host : ', helper.getHost());
+    console.log('content-type : ', headers['content-type']);
+    console.log('apiKey : ', helper.getApiKey());
+    console.log('apiSecret : ', helper.getApiSecret());
+    console.log('requete url : ', helper.getHost() + ':' + '8081' + path);
+    console.log('headers : ', util.inspect(headers, {showHidden: false, depth: null}));
+
+
+    var promise = new Promise(
+        function (resolve, reject) {
+
+            form.pipe(request);
+
+            request.on('response', function (res) {
+                var body = '';
+                if (res.statusCode != 200) {
+                    reject('Error: ' + res.statusCode)
+                }
+                imageStream.on('error', function (err) {
+                    res.end('error imageStream : ', err);
+                });
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+                res.on('end', function () {
+                    resolve(body);
+                });
+            });
+        }
+    );
+    return promise;
 }
 
-function visionectGetMethod(path,expectedHttpCode,contentType){
-  return visionectRequest(path,'GET',contentType,null,expectedHttpCode)
+function visionectGetMethod(path, expectedHttpCode, contentType) {
+    return visionectRequest(path, 'GET', contentType, null, expectedHttpCode)
 }
 
-function visionectPutMethod(path,body,expectedHttpCode,contentType){
-  return visionectRequest(path,'PUT',contentType,body,expectedHttpCode);
+function visionectPutMethod(path, body, expectedHttpCode, contentType) {
+    return visionectRequest(path, 'PUT', contentType, body, expectedHttpCode);
 }
 
-function visionectPostMethod(path,body,expectedHttpCode,contentType){
-  return visionectRequest(path,'POST',contentType,body,expectedHttpCode);
+function visionectPostMethod(path, body, expectedHttpCode, contentType) {
+    return visionectRequest(path, 'POST', contentType, body, expectedHttpCode);
 }
 
-function visionectDeleteMethod(path,contentType,expectedHttpCode){
-  return visionectRequest(path,'DELETE',contentType,null,expectedHttpCode);
+function visionectDeleteMethod(path, expectedHttpCode, contentType) {
+    return visionectRequest(path, 'DELETE', contentType, null, expectedHttpCode);
 }
